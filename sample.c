ff58e6fd436d59e24770c5df85cedbcdadffd9eb
Teresa Tian <41807109+Astatine-213-Tian@users.noreply.github.com>
2025-02-20 20:24:10 -0500

    Update sample.c

--- sample.c
+++ sample.c
@@ -10,7 +10,26 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
+    
+    // Use snprintf instead of strcat for safe string concatenation
     char cmd[BUFSIZE];
-    snprintf(cmd, BUFSIZE, "wc -c < %s", argv[1]);
-    system(cmd);
+    if (snprintf(cmd, BUFSIZE, "wc -c < %s", argv[1]) >= BUFSIZE) {
+        fprintf(stderr, "Command too long\n");
+        return -1;
+    }
+
+    // Execute the command and capture its output
+    FILE *fp = popen(cmd, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error executing command\n");
+        return -1;
+    }
+
+    // Read and print the output
+    char output[BUFSIZE];
+    if (fgets(output, BUFSIZE, fp) != NULL) {
+        printf("%s", output);
+    }
+
+    pclose(fp);
 }
