ce5cbe2fd27ef1d664c1b503d5670db4e7019634
Teresa Tian <41807109+Astatine-213-Tian@users.noreply.github.com>
2025-02-26 13:06:58 -0500

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,22 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <string.h>
+#include <stdlib.h>
     
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
-    }
-    struct stat st;
-    if (stat(argv[1], &st) == 0) {
-        printf("%ld\n", st.st_size);
-    } else {
-        perror("Error accessing file\n");
-        return -1;
-    }
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index-1]);
 }
